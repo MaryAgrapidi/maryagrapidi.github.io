69476ce2ba4522155b824f2c9b61a5c1a348e800
MaryAgrapidi <83584297+MaryAgrapidi@users.noreply.github.com>
2025-02-18 14:46:20 +0200

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -331,7 +331,16 @@
 						$menu._hide();
 
 			});
-
+<script>
+    function toggleQuery(id) {
+        var queryDiv = document.getElementById(id);
+        if (queryDiv.style.display === "none" || queryDiv.style.display === "") {
+            queryDiv.style.display = "block";
+        } else {
+            queryDiv.style.display = "none";
+        }
+    }
+</script>
 })(jQuery);
 
 
